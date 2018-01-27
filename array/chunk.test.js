97cf922f64aa3d6483e0befc8cb661d3ab49a7b4
Michael YAO <yao.developpeur@gmail.com>
2018-01-28 00:02:13 +0100

    (PASS) should return array splited by arrays of size item (Number/String)

--- array/chunk.test.js
+++ array/chunk.test.js
@@ -10,4 +10,7 @@ describe('chunk', () => {
   it('should return array splited by arrays of 1 item with no size argument passed to the function', () => {
     expect(chunk([1,2])).toEqual([[1],[2]]);
   });
+  it('should return array splited by arrays of size item (Number/String)', () => {
+    expect(chunk([1,2], "1")).toEqual([[1],[2]]);
+  });
 });
