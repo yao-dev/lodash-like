90c50b539a15db50c72242c8f1f5a17b948917a0
Michael YAO <yao.developpeur@gmail.com>
2018-01-27 23:47:46 +0100

    (PASS) should return empty array with size argument equal to empty string

--- array/chunk.test.js
+++ array/chunk.test.js
@@ -4,4 +4,7 @@ describe('chunk', () => {
   it('should return empty array with size argument equal to 0', () => {
     expect(chunk([1,2,3,4,5], 0)).toEqual([]);
   });
+  it('should return empty array with size argument equal to empty string', () => {
+    expect(chunk([1,2,3,4,5], "")).toEqual([]);
+  });
 });
